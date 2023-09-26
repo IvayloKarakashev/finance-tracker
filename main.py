29e09e2d8d572da9fb1ee0bb8ae1518cf5d9a486
Ivaylо <ivaylo.a.karakashev@gmail.com>
2023-09-26 22:48:27 +0300

    Implemented navigation

--- main.py
+++ main.py
@@ -2,10 +2,83 @@ import sys
 
 from PyQt5.QtCore import Qt, QAbstractListModel, QModelIndex
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QListView, QLineEdit, QListWidget, QDialog, \
-    QDialogButtonBox
+    QDialogButtonBox, QStackedWidget, QFrame, QWidget
 from PyQt5 import uic, QtGui
 
 from expense_categories import ExpenseCategory
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.central_widget = QStackedWidget()
+        self.setCentralWidget(self.central_widget)
+
+        self.dashboard_page = Dashboard()
+        # uic.loadUI('finance_tracker.ui', self)
+        self.central_widget.addWidget(self.dashboard_page)
+        self.central_widget.setCurrentWidget(self.dashboard_page)
+
+        self.nav_menu = NavWidget()
+        self.central_widget.addWidget(self.nav_menu)
+        uic.loadUi('navigation.ui', self)
+
+        self.expense_categories_page = ExpenseCategories()
+        self.central_widget.addWidget(self.expense_categories_page)
+
+        # Navigation
+        self.dashboard_button = self.findChild(QPushButton, "dashboardButton")
+        self.expense_categories_button = self.findChild(QPushButton, "expenseCategoriesButton")
+
+        self.dashboard_button.clicked.connect(self.go_to_dashboard)
+        self.expense_categories_button.clicked.connect(self.go_to_expense_categories)
+
+        # self.setup_ui()  # Initialize UI elements
+
+        # TODO: Move these buttons to their respective views(classes)
+        self.expenseCategoriesList = self.findChild(QListView, "expenseCategoriesList")
+        self.expenseCategoryNameInput = self.findChild(QLineEdit, "expenseCategoryNameInput")
+        self.addExpenseCategoryButton = self.findChild(QPushButton, "addExpenseCategoryButton")
+
+    def go_to_dashboard(self):
+        self.central_widget.setCurrentWidget(self.dashboard_page)
+
+    def go_to_expense_categories(self):
+        self.central_widget.setCurrentWidget(self.expense_categories_page)
+
+        #  TODO: Implement these former setup_ui() features
+        # Expense categories layout features
+        # self.model = ExpenseCategoryModel()
+        # self.expenseCategoriesList.setModel(self.model)
+        # self.addExpenseCategoryButton.clicked.connect(self.open_new_category_dialog)
+
+        # self.dashboardButton.clicked.connect(lambda: self.switch_to_layout(self.finance_tracker_layout))
+        # self.expenseCategoriesButton.clicked.connect(lambda: self.switch_to_layout(self.expense_categories_layout))
+
+    # def switch_to_layout(self, layout):
+    #     self.central_widget.setCurrentWidget(uic.loadUi(layout))
+
+    # def open_new_category_dialog(self):
+    #     dialog = AddCategoryDialog(self.model)
+    #     dialog.exec_()
+
+
+class Dashboard(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        uic.loadUi('finance_tracker.ui', self)
+
+
+class ExpenseCategories(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        uic.loadUi('expense_categories.ui', self)
+
+
+class NavWidget(QFrame):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('navigation.ui', self)
 
 
 class ExpenseCategoryModel(QAbstractListModel):
@@ -62,63 +135,6 @@ class AddCategoryDialog(QDialog):
             print("Category already exists.")
 
         self.nameLineEdit.clear()
-
-
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setup_ui()  # Initialize UI elements
-
-        # Define elements
-        self.expenseCategoriesList = self.findChild(QListView, "expenseCategoriesList")
-
-        self.dashboardButton = self.findChild(QPushButton, "dashboardButton")
-        self.expenseCategoriesButton = self.findChild(QPushButton, "expenseCategoriesButton")
-        self.expenseCategoryNameInput = self.findChild(QLineEdit, "expenseCategoryNameInput")
-        self.addExpenseCategoryButton = self.findChild(QPushButton, "addExpenseCategoryButton")
-
-
-        # self.addExpenseCategoryButton.clicked.connect(self.add_expense_category)
-
-    # def add_expense_category(self):
-    #     category_name_input = self.expenseCategoryNameInput.text()
-    #
-    #     try:
-    #         if category_name_input:
-    #             # Check if the category already exists in the model
-    #             if not any(cat.name == category_name_input for cat in self.model.categories):
-    #                 # Create an ExpenseCategory object
-    #                 expense_category = ExpenseCategory(category_name_input)
-    #
-    #                 # Insert a row into the model
-    #                 self.model.insertRow(len(self.model.categories))
-    #                 index = self.model.index(len(self.model.categories) - 1)
-    #                 self.model.setData(index, expense_category, Qt.DisplayRole)
-    #                 self.model.categories.append(expense_category)
-    #                 self.model.layoutChanged.emit()
-    #                 self.expenseCategoriesList.setModel(self.model)
-    #
-    #                 print(self.model.categories)
-    #             else:
-    #                 print("Category already exists.")
-    #     except Exception as e:
-    #         # Handle exceptions here (e.g., display an error message)
-    #         print(f"An error occurred: {e}")
-
-        # self.expenseCategoryNameInput.clear()
-
-        # Clear the QLineEdit after adding the category
-
-    def setup_ui(self):
-        uic.loadUi("expense_categories.ui", self)  # Load the .ui file and set up UI elements
-        self.model = ExpenseCategoryModel()
-        self.expenseCategoriesList.setModel(self.model)
-        self.addExpenseCategoryButton.clicked.connect(self.open_new_category_dialog)
-
-    def open_new_category_dialog(self):
-        dialog = AddCategoryDialog(self.model)
-        dialog.exec_()
 
 
 if __name__ == "__main__":
